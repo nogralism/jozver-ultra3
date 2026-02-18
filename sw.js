b332a610e8ccd09f98f1aa82c4149151b8be9e70
nogralism <rdadashi5757@gmail.com>
2026-02-18 05:09:46 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
 const CACHE_NAME = 'jozve-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './icon.png'
+  '/jozver-ultra3/',
+  '/jozver-ultra3/index.html',
+  '/jozver-ultra3/icon.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
-
